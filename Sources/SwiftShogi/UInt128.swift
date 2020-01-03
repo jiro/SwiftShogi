cab0940fff8aa77b255cd66e5661b676a8a0157e
Jiro Nagashima <hedjirog@users.noreply.github.com>
2020-01-03 11:31:10 +0900

    Conform UInt128 to ExpressibleByIntegerLiteral

--- Sources/SwiftShogi/UInt128.swift
+++ Sources/SwiftShogi/UInt128.swift
@@ -2,3 +2,13 @@ struct UInt128 {
     var upperBits: UInt64
     var lowerBits: UInt64
 }
+
+extension UInt128: ExpressibleByIntegerLiteral {
+    init(integerLiteral value: UInt64) {
+        self.init(value)
+    }
+
+    init<T>(_ source: T) where T : BinaryInteger {
+        self.init(upperBits: 0, lowerBits: UInt64(source))
+    }
+}
