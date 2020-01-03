bdafefb3de7f626bfcfe87cf72eebf1573ef809a
Jiro Nagashima <hedjirog@users.noreply.github.com>
2020-01-03 22:13:21 +0900

    Add subscript to Board

--- Sources/SwiftShogi/Board.swift
+++ Sources/SwiftShogi/Board.swift
@@ -1,7 +1,40 @@
+/// A shogi board used to map `Square`s to `Piece`s.
 public struct Board {
-    private(set) var pieceBitboards: [Piece: Bitboard]
+    private var pieceBitboards: [Piece: Bitboard]
 
+    /// Creates a shogi board.
     public init() {
-        pieceBitboards = Dictionary(uniqueKeysWithValues: Piece.allCases.map { ($0, Bitboard(rawValue: 0)) })
+        let piecesAndBitboards = Piece.allCases.map { ($0, Bitboard(rawValue: 0)) }
+        pieceBitboards = Dictionary(uniqueKeysWithValues: piecesAndBitboards)
+    }
+}
+
+extension Board {
+    
+    /// Gets and sets a piece at `square`.
+    public subscript(square: Square) -> Piece? {
+        get {
+            allPieces.first { exists($0, at: square) }
+        }
+        set(piece) {
+            allPieces.forEach { remove($0, at: square) }
+            if let piece = piece {
+                insert(piece, at: square)
+            }
+        }
+    }
+
+    private var allPieces: [Piece] { Array(pieceBitboards.keys) }
+
+    private func exists(_ piece: Piece, at square: Square) -> Bool {
+        return pieceBitboards[piece]![square]
+    }
+
+    private mutating func insert(_ piece: Piece, at square: Square) {
+        pieceBitboards[piece]![square] = true
+    }
+
+    private mutating func remove(_ piece: Piece, at square: Square) {
+        pieceBitboards[piece]![square] = false
     }
 }
--- Tests/SwiftShogiTests/BoardTests.swift
+++ Tests/SwiftShogiTests/BoardTests.swift
@@ -2,4 +2,15 @@ import XCTest
 @testable import SwiftShogi
 
 final class BoardTests: XCTestCase {
+    func testSubscript() {
+        let piece = Piece(kind: .king, color: .black)
+        var board = Board()
+        XCTAssertNil(board[.oneA])
+
+        board[.oneA] = piece
+        XCTAssertEqual(board[.oneA], piece)
+
+        board[.oneA] = nil
+        XCTAssertNil(board[.oneA])
+    }
 }
