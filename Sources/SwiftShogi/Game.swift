41941555350fede88efa104970c9801ce16fa8d8
Jiro Nagashima <hedjirog@users.noreply.github.com>
2020-01-04 00:07:33 +0900

    Add methods to perform move

--- Sources/SwiftShogi/Game.swift
+++ Sources/SwiftShogi/Game.swift
@@ -9,3 +9,41 @@ public struct Game {
         self.capturedPieces = capturedPieces
     }
 }
+
+extension Game {
+
+    /// Performs `move` with validating it.
+    public mutating func perform(_ move: Move) throws {
+        try validate(move)
+
+        let sourcePiece = remove(from: move.source)
+        insert(sourcePiece, to: move.destination)
+    }
+
+    private mutating func remove(from source: Move.Source) -> Piece {
+        switch source {
+        case let .board(square):
+            let piece = board[square]!
+            board[square] = nil
+            return piece
+        case let .capturedPiece(piece):
+            let index = capturedPieces.firstIndex(of: piece)!
+            capturedPieces.remove(at: index)
+            return piece
+        }
+    }
+
+    private mutating func insert(_ piece: Piece, to destination: Move.Destination) {
+        switch destination {
+        case let .board(square):
+            board[square] = piece
+        }
+    }
+}
+
+extension Game {
+
+    /// Validates `move`.
+    public func validate(_ move: Move) throws {
+    }
+}
--- Tests/SwiftShogiTests/GameTests.swift
+++ Tests/SwiftShogiTests/GameTests.swift
@@ -2,4 +2,37 @@ import XCTest
 @testable import SwiftShogi
 
 final class GameTests: XCTestCase {
+    func testPerformMoveFromBoard() {
+        let piece = Piece(kind: .gold, color: .black)
+        let board = Board(pieces: [.oneA: piece])
+        var game = Game(board: board)
+        XCTAssertEqual(game.board[.oneA], piece)
+        XCTAssertNil(game.board[.oneB])
+
+        let move = Move(source: .board(.oneA), destination: .board(.oneB))
+        XCTAssertNoThrow(try game.perform(move))
+
+        XCTAssertNil(game.board[.oneA])
+        XCTAssertEqual(game.board[.oneB], piece)
+    }
+
+    func testPerformMoveFromCapturedPiece() {
+        let piece = Piece(kind: .gold, color: .black)
+        var game = Game(capturedPieces: [piece])
+        XCTAssertNil(game.board[.oneA])
+        XCTAssertTrue(game.capturedPieces.contains(piece))
+
+        let move = Move(source: .capturedPiece(piece), destination: .board(.oneA))
+        XCTAssertNoThrow(try game.perform(move))
+
+        XCTAssertEqual(game.board[.oneA], piece)
+        XCTAssertFalse(game.capturedPieces.contains(piece))
+    }
+}
+
+private extension Board {
+    init(pieces: [Square: Piece]) {
+        self.init()
+        pieces.forEach { square, piece in self[square] = piece }
+    }
 }
