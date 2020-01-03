156262d14812a0d9196da73a01b67c5a0dd93c84
Jiro Nagashima <hedjirog@users.noreply.github.com>
2020-01-03 21:49:42 +0900

    Add subscript to Bitboard

--- Sources/SwiftShogi/Bitboard.swift
+++ Sources/SwiftShogi/Bitboard.swift
@@ -1,7 +1,35 @@
+/// A bitmap of eighty-one bits suitable for storing squares for various pieces.
+///
+/// The first bit refers to `Square.oneA`, and the last (81th) bit refers to `Square.nineI`.
 struct Bitboard: RawRepresentable {
     private(set) var rawValue: UInt128
 
     init(rawValue: UInt128) {
         self.rawValue = rawValue
+    }
+}
+
+extension Bitboard {
+
+    /// The `Bool` value for the bit at `square`.
+    subscript(square: Square) -> Bool {
+        get {
+            intersects(Self(square: square))
+        }
+        set(hasBit) {
+            if hasBit {
+                rawValue |= Self(square: square).rawValue
+            } else {
+                rawValue &= ~Self(square: square).rawValue
+            }
+        }
+    }
+
+    private init(square: Square) {
+        self.init(rawValue: 1 << square.rawValue)
+    }
+
+    private func intersects(_ other: Self) -> Bool {
+        rawValue & other.rawValue != 0
     }
 }
--- Tests/SwiftShogiTests/BitboardTests.swift
+++ Tests/SwiftShogiTests/BitboardTests.swift
@@ -2,4 +2,14 @@ import XCTest
 @testable import SwiftShogi
 
 final class BitboardTests: XCTestCase {
+    func testSubscript() {
+        var bitboard = Bitboard(rawValue: 0)
+        XCTAssertFalse(bitboard[.oneA])
+
+        bitboard[.oneA] = true
+        XCTAssertTrue(bitboard[.oneA])
+
+        bitboard[.oneA] = false
+        XCTAssertFalse(bitboard[.oneA])
+    }
 }
